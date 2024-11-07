06593f0eff37d66fb05b71fe2787f0de9195791b
Joe Rinella <joseph_p_rinella@progressive.com>
2024-11-07 14:30:43 -0500

    step 2

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,4 +1,7 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
 
 @Component({
     selector: 'app-books',
@@ -6,10 +9,41 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
     changeDetection: ChangeDetectionStrategy.OnPush,
     imports: [],
     template: `
-        <p>Books goes here</p>
+        <div class="overflow-x-auto">
+            <table class="table">
+                <!-- head -->
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Title</th>
+                        <th>Author</th>
+                        <th>Year</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @for(book of clientResponse(); track book.id) {
+                        <tr>
+                            <td>{{book.id}}</td>
+                            <td>{{book.title}}</td>
+                            <td>{{book.author}}</td>
+                            <td>{{book.year}}</td>
+                        </tr>
+                    }
+                </tbody>
+            </table>
+        </div>
     `,
     styles: ``
 })
 export class BooksComponent {
-
+    #httpClient$ = inject(HttpClient);
+    clientResponse = toSignal(this.#httpClient$.get<{
+        data: { 
+            id: string; 
+            title: string; 
+            author: string; 
+            year: number;
+        }[];
+    }>('/api/books')
+        .pipe(map((res) => res.data)));
 }
